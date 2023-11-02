d040b3d8e1092e2f61d165bcfbd8785d684c3b7b
Alexandr Gritsunov <ickpimo@gmail.com>
2023-11-02 11:19:17 +0300

    queries completed

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,15 +2,44 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
+SELECT customers.company_name, concat(employees.first_name, ' ', employees.last_name) as employee
+FROM orders
+JOIN shippers ON orders.ship_via=shippers.shipper_id
+JOIN customers USING(customer_id)
+JOIN employees USING(employee_id)
+WHERE employee_id=orders.employee_id AND customers.city=employees.city AND shippers.company_name='United Package'
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
 
+SELECT product_name, units_in_stock, contact_name, phone
+FROM products
+JOIN suppliers USING(supplier_id)
+JOIN categories USING(category_id)
+WHERE products.discontinued=0 AND units_in_stock < 25 AND category_name IN ('Dairy Products', 'Condiments')
+ORDER BY units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
+SELECT company_name
+FROM customers
+WHERE customer_id not in (SELECT customer_id FROM orders)
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+
+SELECT DISTINCT product_name
+FROM products
+WHERE EXISTS (SELECT product_id FROM order_details WHERE order_details.product_id=products.product_id AND quantity=10)
+
+
+SELECT DISTINCT product_name
+FROM products
+WHERE product_id = ANY (SELECT product_id FROM order_details WHERE quantity=10)
+
+
+SELECT product_name
+FROM products
+WHERE product_id = ANY (SELECT product_id FROM order_details WHERE quantity = 10)
